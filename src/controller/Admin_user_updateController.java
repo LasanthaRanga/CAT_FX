a62f5ce09cd57112e3046ae325e6a110a22e67f7
suhadaprasanna <suhadaprasanna@gmail.com>
2018-01-20 18:05:21 +0530

    asd

--- src/controller/Admin_user_updateController.java
+++ src/controller/Admin_user_updateController.java
@@ -5,16 +5,22 @@
  */
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 import pojo.User;
 
 /**
@@ -32,15 +38,25 @@ public class Admin_user_updateController implements Initializable {
     private TableColumn<pojo.User, String> tbl_clmn_name;
     @FXML
     private TableColumn<pojo.User, String> tbl_clmn_contact;
-    
+
     private modle.Users user;
-    
+    @FXML
+    private JFXTextField txt_fname;
+    @FXML
+    private JFXTextField txt_nic;
+    @FXML
+    private JFXTextField txt_mobile;
+    @FXML
+    private JFXTextField txt_regisdate;
+    @FXML
+    private JFXButton btn_deactive;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        user=new modle.Users();
+        user = new modle.Users();
         ObservableList<pojo.User> list_active_users = FXCollections.observableArrayList(user.getActiveUsers());
         tbl_clmn_name.setCellValueFactory(new PropertyValueFactory<>("fullName"));
         tbl_clmn_nic.setCellValueFactory(new PropertyValueFactory<>("nic"));
@@ -52,7 +68,47 @@ public class Admin_user_updateController implements Initializable {
     private void loadUser(MouseEvent event) {
         Integer idUser = tbl_user.getSelectionModel().getSelectedItem().getIdUser();
         User byIdUser = user.getByIdUser(0);
-        
+        if (byIdUser != null) {
+            txt_fname.setText(byIdUser.getFullName());
+            txt_mobile.setText(byIdUser.getMobile());
+            txt_nic.setText(byIdUser.getNic());
+            txt_regisdate.setText(new SimpleDateFormat("yyyy-MM-dd").format(byIdUser.getRegDate()));
+        } else {
+            Notifications.create()
+                    .title("Warning")
+                    .text("Not Found User.")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.BOTTOM_RIGHT).showWarning();
+        }
+
     }
-    
+
+    @FXML
+    private void deactiveUser(MouseEvent event) {
+        Integer idUser = tbl_user.getSelectionModel().getSelectedItem().getIdUser();
+        User byIdUser = user.getByIdUser(0);
+        if (byIdUser != null) {
+            byIdUser.setStatus(0);
+            if(user.update(byIdUser)){
+                Notifications.create()
+                    .title("Success")
+                    .text("Deactivation Success.")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.BOTTOM_RIGHT).showInformation();
+            }else{
+                Notifications.create()
+                    .title("Warning")
+                    .text("Deactivation failed.")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.BOTTOM_RIGHT).showWarning();
+            }
+        } else {
+            Notifications.create()
+                    .title("Warning")
+                    .text("Not Found User.")
+                    .hideAfter(Duration.seconds(3))
+                    .position(Pos.BOTTOM_RIGHT).showWarning();
+        }
+    }
+
 }

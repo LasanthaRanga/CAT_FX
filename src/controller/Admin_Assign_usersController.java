4a6052fb9aa109fbf1470d1d1a76b96bb4897fe2
LasanthaRanga <rm.lasantharanga@gmail.com>
2018-01-20 10:31:59 +0530

    push

--- src/controller/Admin_Assign_usersController.java
+++ src/controller/Admin_Assign_usersController.java
@@ -1,9 +1,11 @@
-
 package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -12,12 +14,33 @@ import javafx.fxml.Initializable;
  */
 public class Admin_Assign_usersController implements Initializable {
 
+    @FXML
+    private TableView<?> tbl_user;
+
+    @FXML
+    private TableColumn<?, ?> col_idUser;
+
+    @FXML
+    private TableColumn<?, ?> col_fullname;
+
+    @FXML
+    private TableColumn<?, ?> col_nic;
+
+    @FXML
+    private TableColumn<?, ?> col_mobile;
+
+    @FXML
+    private TableColumn<?, ?> col_date;
+
+    @FXML
+    private TableColumn<?, ?> col_statues;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }

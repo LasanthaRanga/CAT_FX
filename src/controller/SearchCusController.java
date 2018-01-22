666581ed73a4369e16b29ecddf87a8de6f4fce72
LasanthaRanga <rm.lasantharanga@gmail.com>
2018-01-22 14:08:21 +0530

    asdf

--- src/controller/SearchCusController.java
+++ src/controller/SearchCusController.java
@@ -5,11 +5,16 @@ import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import modle.Customer;
+import modle.Ward;
 
 /**
  * FXML Controller class
@@ -22,22 +27,22 @@ public class SearchCusController implements Initializable {
     private JFXTextField txt_fullname;
 
     @FXML
-    private TableView<?> tbl_cus;
+    private TableView<cus> tbl_cus;
 
     @FXML
-    private TableColumn<?, ?> c_id;
+    private TableColumn<cus, String> c_id;
 
     @FXML
-    private TableColumn<?, ?> c_nic;
+    private TableColumn<cus, String> c_nic;
 
     @FXML
-    private TableColumn<?, ?> c_address;
+    private TableColumn<cus, String> c_address;
 
     @FXML
-    private TableColumn<?, ?> c_mobile;
+    private TableColumn<cus, String> c_mobile;
 
     @FXML
-    private TableColumn<?, ?> c_email;
+    private TableColumn<cus, String> c_email;
 
     @FXML
     private JFXTextField txt_idCus;
@@ -50,10 +55,79 @@ public class SearchCusController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        List<Customer> cuslist = modle.StaticBadu.getCuslist();
-        for (Customer customer : cuslist) {
-            System.out.println(customer.getIdCustomer());
+        loadTable();
+    }
+
+    public class cus {
+
+        public cus(int idcus, String nic, String address, String mobile, String email) {
+            this.idcus = idcus;
+            this.nic = new SimpleStringProperty(nic);
+            this.address = new SimpleStringProperty(address);
+            this.mobile = new SimpleStringProperty(mobile);
+            this.email = new SimpleStringProperty(email);
         }
+
+        /**
+         * @return the idcus
+         */
+        public int getIdcus() {
+            return idcus;
+        }
+
+        /**
+         * @return the nic
+         */
+        public String getNic() {
+            return nic.get();
+        }
+
+        /**
+         * @return the address
+         */
+        public String getAddress() {
+            return address.get();
+        }
+
+        /**
+         * @return the mobile
+         */
+        public String getMobile() {
+            return mobile.get();
+        }
+
+        /**
+         * @return the email
+         */
+        public String getEmail() {
+            return email.get();
+        }
+        private int idcus;
+        private SimpleStringProperty nic;
+        private SimpleStringProperty address;
+        private SimpleStringProperty mobile;
+        private SimpleStringProperty email;
+
+    }
+    ObservableList List = FXCollections.observableArrayList();
+
+    public void loadTable() {
+        c_id.setCellValueFactory(new PropertyValueFactory<>("idcus"));
+        c_nic.setCellValueFactory(new PropertyValueFactory<>("nic"));
+        c_address.setCellValueFactory(new PropertyValueFactory<>("address"));
+        c_mobile.setCellValueFactory(new PropertyValueFactory<>("mobile"));
+        c_email.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        List<Customer> cuslist = modle.StaticBadu.getCuslist();
+        List.clear();
+        for (Customer customer : cuslist) {
+            txt_fullname.setText(customer.getFullName());
+            String addr = customer.getAddress1() + ",  " + customer.getAddress2() + ",  " + customer.getAddress3();
+            List.add(new cus(customer.getIdCustomer(), customer.getNic(), addr, customer.getMobile(), customer.getEmail()));
+        }
+
+        tbl_cus.setItems(List);
+
     }
 
 }

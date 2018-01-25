d287626b53bf71cdbed31e07d524fdf84cce899c
suhadaprasanna <suhadaprasanna@gmail.com>
2018-01-25 09:29:41 +0530

    ad

--- src/modle/Report.java
+++ src/modle/Report.java
@@ -27,30 +27,30 @@ import org.hibernate.engine.SessionFactoryImplementor;
  */
 public class Report {
 
-    private Connection getConnection() {
-        try {
-            return ((SessionFactoryImplementor) conn.NewHibernateUtil.getSessionFactory()).getConnectionProvider().getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
-
-    public void paymentReceipt(int paymentId) {
-        try {
-            String path = "C:\\cat\\report\\paymentReceipt.jrxml";
-            JasperReport jr = JasperCompileManager.compileReport(path);
-            HashMap param=new HashMap<String, Integer>();
-            param.put("paymentId", paymentId);
-            JasperPrint jp = JasperFillManager.fillReport(jr, param, this.getConnection());
-            JasperViewer.viewReport(jp);
-        } catch (JRException jRException) {
-            Notifications.create()
-                    .title("Warning")
-                    .text("Can not generate report. Something went wrong.\n(" + jRException.getMessage() + ")")
-                    .hideAfter(Duration.seconds(3))
-                    .position(Pos.BOTTOM_RIGHT).showWarning();
-        }
-    }
+//    private Connection getConnection() {
+//        try {
+//            return ((SessionFactoryImplementor) conn.NewHibernateUtil.getSessionFactory()).getConnectionProvider().getConnection();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(Report.class.getName()).log(Level.SEVERE, null, ex);
+//            return null;
+//        }
+//    }
+//
+//    public void paymentReceipt(int paymentId) {
+//        try {
+//            String path = "C:\\cat\\report\\paymentReceipt.jrxml";
+//            JasperReport jr = JasperCompileManager.compileReport(path);
+//            HashMap param=new HashMap<String, Integer>();
+//            param.put("paymentId", paymentId);
+//            JasperPrint jp = JasperFillManager.fillReport(jr, param, this.getConnection());
+//            JasperViewer.viewReport(jp);
+//        } catch (JRException jRException) {
+//            Notifications.create()
+//                    .title("Warning")
+//                    .text("Can not generate report. Something went wrong.\n(" + jRException.getMessage() + ")")
+//                    .hideAfter(Duration.seconds(3))
+//                    .position(Pos.BOTTOM_RIGHT).showWarning();
+//        }
+//    }
 
 }

92f96f58aa1482f39938cdbcb52b4bab8fd7b115
suhadaprasanna <suhadaprasanna@gmail.com>
2018-01-25 09:48:59 +0530

    asd

--- src/modle/Report.java
+++ src/modle/Report.java
@@ -5,21 +5,7 @@
  */
 package modle;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.geometry.Pos;
-import javafx.util.Duration;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.view.JasperViewer;
-import org.controlsfx.control.Notifications;
-import org.hibernate.engine.SessionFactoryImplementor;
+
 
 /**
  *

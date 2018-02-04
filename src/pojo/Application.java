7a0153d2ffadcb120b0d1d78f5568563e0a62207
LasanthaRanga <rm.lasantharanga@gmail.com>
2018-02-04 16:00:38 +0530

    jj

--- src/pojo/Application.java
+++ src/pojo/Application.java
@@ -1,5 +1,5 @@
 package pojo;
-// Generated Jan 30, 2018 12:46:38 PM by Hibernate Tools 4.3.1
+// Generated Feb 4, 2018 3:51:56 PM by Hibernate Tools 4.3.1
 
 
 import java.util.Date;
--- src/pojo/Assessment.java
+++ src/pojo/Assessment.java
@@ -1,5 +1,5 @@
 package pojo;
-// Generated Jan 30, 2018 11:20:06 AM by Hibernate Tools 4.3.1
+// Generated Feb 4, 2018 3:51:56 PM by Hibernate Tools 4.3.1
 
 
 import java.util.HashSet;
